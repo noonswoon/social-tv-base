9c423f7ab8a7e0da810ad30e6cec3292da108ca6
Panisa <pokepokeru@hotmail.com>
2012-05-08 14:37:21 +0700

    Profile template

--- Resources/ui/common/Pf_ProfileHeader.js
+++ Resources/ui/common/Pf_ProfileHeader.js
@@ -5,97 +5,91 @@ var ProfileHeaderView = function(){
         	type: 'linear',
         	startPoint: { x: '0%', y: '0%' },
         	endPoint: { x: '0%', y: '100%' },
-        	colors: [ { color: '#fff', offset: 0.0}, { color: '#D1CBCD', offset: 1.0 } ],
-   			}
+        	colors: [ { color: '#fff', offset: 0.0}, { color: '#D1CBCD', offset: 1.0 } ]}
 	});
-	
-	headerView.thumbnail = Ti.UI.createImageView({
-		image: 'images/kuma100x100.png',
-		top: 10, left: 10,
-		width: 100,
-		height: 100,
-		border: 1,
-		borderColor: '#999',
-		backgroundColor: '#E2E5EE'	
-	});
-headerView.add(headerView.thumbnail);
-	
-	headerView.nameLabel = Ti.UI.createLabel({
-		text: 'Jaew Panisa', //name
-		top: 5,
-		left: 120,
-		width: 'auto',
-		height: 30,
-		font: { fontWeight: 'bold', fontSize: 14}
-	})
-headerView.add(headerView.nameLabel);
-//switch: login via ?
-	headerView.login = Ti.UI.createView({
-		top: 40,
-		left: 120,
-		width: 60,
-		height: 70,
-		border:1,
-		borderColor: '#999'
-	});
-headerView.add(headerView.login);
 
-	//number of like
-	headerView.like = Ti.UI.createView({
-		top: 40,
-		left: 185,
-		width: 60,
-		height: 70,
-		backgroundColor: '#999',
-		opacity: 0.9
-	});
-	//	img
-	headerView.like.likeImage = Ti.UI.createImageView({
-		image: 'images/KS_nav_ui.png',
-		width: 30,
-		height: 30,
-		top: 3
-	});
-	// count
-	headerView.like.likeCount = Ti.UI.createLabel({
-		text: '59',
-		font: {fontSize: 26, fontStyle: 'bold'},
-		color: '#fff',
-		top: 30
-	});
-	headerView.like.add(headerView.like.likeImage);
-	headerView.like.add(headerView.like.likeCount);
+		var profilePicture = Ti.UI.createImageView({
+			image: 'images/kuma100x100.png',
+			top: 10, left: 10,
+			width: 100,
+			height: 100,
+			border: 1,
+			borderColor: '#999',
+			backgroundColor: '#E2E5EE'	
+		});
+		var profileName = Ti.UI.createLabel({
+			text: 'Jaew Panisa', //name
+			top: 5,
+			left: 120,
+			width: 'auto',
+			height: 30,
+			font: { fontWeight: 'bold', fontSize: 14}
+		})
+		//switch: login via ?
+		var viaLogin = Ti.UI.createView({
+			top: 40,
+			left: 120,
+			width: 60,
+			height: 70,
+			border:1,
+			borderColor: '#999'
+		});
+		//number of like
+		var columnLike = Ti.UI.createView({
+			top: 40,
+			left: 185,
+			width: 60,
+			height: 70,
+			backgroundColor: '#999',
+			opacity: 0.9
+		});
+			//img
+			var columnLikeImage = Ti.UI.createImageView({
+				image: 'images/KS_nav_ui.png',
+				width: 30,
+				height: 30,
+				top: 3
+			});
+			//count
+			var columnLikeCount = Ti.UI.createLabel({
+				text: '59',
+				font: {fontSize: 26, fontStyle: 'bold'},
+				color: '#fff',
+				top: 30
+			});
+		//number of post
+		var columnPost = Ti.UI.createView({
+			top: 40,
+			left: 250,
+			width: 60,
+			height: 70,
+			backgroundColor: '#999',
+			opacity: 0.9
+		});
+			//	img
+			var columnPostImage = Ti.UI.createImageView({
+				image: 'images/KS_nav_views.png',
+				width: 30,
+				height: 30,
+				top: 3
+			});
+			// count
+			var columnPostCount = Ti.UI.createLabel({
+				text: '27',
+				font: {fontSize: 26, fontStyle: 'bold'},
+				color: '#fff',
+				top: 30
+			});
 
-headerView.add(headerView.like);
-		
-	//number of post
-	headerView.post = Ti.UI.createView({
-		top: 40,
-		left: 250,
-		width: 60,
-		height: 70,
-		backgroundColor: '#999',
-		opacity: 0.9
-	});
-	//	img
-	headerView.post.postImage = Ti.UI.createImageView({
-		image: 'images/KS_nav_views.png',
-		width: 30,
-		height: 30,
-		top: 3
-	});
-	// count
-	headerView.post.postCount = Ti.UI.createLabel({
-		text: '27',
-		font: {fontSize: 26, fontStyle: 'bold'},
-		color: '#fff',
-		top: 30
-	});
-	
-	headerView.post.add(headerView.post.postImage);
-	headerView.post.add(headerView.post.postCount);
-
-headerView.add(headerView.post);		
+	headerView.add(profilePicture);
+	headerView.add(profileName);
+	headerView.add(viaLogin);
+		columnLike.add(columnLikeImage);
+		columnLike.add(columnLikeCount);
+	headerView.add(columnLike);
+		columnPost.add(columnPostImage);
+		columnPost.add(columnPostCount);
+	headerView.add(columnPost);		
 
 return headerView;
 
--- Resources/ui/common/Pf_ProfileMainWindow.js
+++ Resources/ui/common/Pf_ProfileMainWindow.js
@@ -2,63 +2,36 @@ function ProfileMainWindow() {
 	var self = Titanium.UI.createWindow({
 		backgroundColor:'#fff',
 		title: "My Profile",
-		//barColor: '#61A598'
 	});
-/////////////////////////////////////
-//NAVIGATION BAR//	
 	
-//HEADER//
-var ProfileHeader = require('ui/common/Pf_ProfileHeader');
-var profileHeader = new ProfileHeader();
-var headerView = Ti.UI.createView({
-	height: 120});
-headerView.add(profileHeader);
-
-//MENU//	
-var ProfileMenu = require('ui/common/Pf_ProfileMenu');
-var profileMenu = new ProfileMenu();
-var menuView = Ti.UI.createView({
-	height:50
-});
-
-menuView.add(profileMenu);
-
-
-//DETAIL//
-var ProfileDetail = require('ui/common/Pf_ProfileDetail');
-var profileDetail = new ProfileDetail();
-var detailView = Ti.UI.createView({
-	height: 198
-});
-detailView.add(profileDetail);
-
-//no-edit! code:D//
-var data=[];
-
-var header = Ti.UI.createTableViewSection();
-header.headerView = headerView;
-data[0] = header;
-
-var menu = Ti.UI.createTableViewSection();
-menu.headerView = menuView;
-data[1] = menu;
-
-var detail = Ti.UI.createTableViewSection();
-detail.headerView = detailView;
-data[2] = detail;
-
-var table = Ti.UI.createTableView({
-		top: 0,
-		left: 0,
-		right: 0,
-		bottom: 0,
-		scrollable: false,
-		data: data
-	});
-
-	self.add(table);
+//REQUIRE//
+		var ProfileHeader = require('ui/common/Pf_ProfileHeader');
+		var Detail = require('ui/common/Pf_ProfileDetails');
+//UI//
+		var headerView = Ti.UI.createView({
+				height: 120
+		});	
+		var UserProfileTable = Ti.UI.createTableView({
+				top: 0,
+				left: 0,
+				right: 0,
+				bottom: 0,
+				scrollable: false,
+			});
+		var header = Ti.UI.createTableViewSection();
+		var profileHeader = new ProfileHeader();
+		var detail = new Detail();
+		var UserProfile=[];
+		
+		headerView.add(profileHeader);
+		
+		header.headerView = headerView;
+		
+		UserProfile.push(header);
+		UserProfile.push(detail);
+		UserProfileTable.setData(UserProfile);
 	
-/////////////////////////////////////	
+	self.add(UserProfileTable);		
 	return self;
 }
 

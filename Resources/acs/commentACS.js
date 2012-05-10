484dad7c1c86ae197fc12f3ab591ab606c9d6ee5
Kavin Mickey Asavanant <akavin@gmail.com>
2012-05-11 03:55:43 +0700

    still in progress

--- Resources/acs/commentACS.js
+++ Resources/acs/commentACS.js
@@ -1,5 +1,8 @@
 exports.commentACS_fetchAllCommentsOfPostId = function(_topicId) {
 	var commentsOfPost = [];
+	var commentIdsWithRatingsOrComments = [];
+	var commentsOfComments = [];
+	//getting comments on the topic
 	Cloud.Reviews.query({
 	    post_id: _topicId,
 	    page: 1,
@@ -14,14 +17,51 @@ exports.commentACS_fetchAllCommentsOfPostId = function(_topicId) {
 	            	id: review.id,
 	            	topic_id: _topicId,
 	            	content: review.content,
-	            	rating: review.rating,
 	            	user:review.user,
 	            	response_to_object_id: review.custom_fields.response_to_object_id,
 	            	updated_at: review.updated_at
-	            }
+	            };
 				commentsOfPost.push(curComment);
-	        }
-	       Ti.App.fireEvent("commentsLoadedComplete",{fetchedComments:commentsOfPost});
+				if(review.reviews_count !== undefined) {
+	            	commentIdsWithRatingsOrComments.push(review.id);
+	            } 
+			}
+			
+			if(commentIdsWithRatingsOrComments.length == 0)
+				Ti.App.fireEvent("commentsLoadedComplete",{fetchedComments:commentsOfPost, fetchedCommentsOfComments:commentsOfComments});
+			
+			//getting comments/rating of comment
+			for(var i=0; i <commentIdsWithRatingsOrComments.length;i++) {
+				var curCommentId = commentIdsWithRatingsOrComments[i];
+				Cloud.Reviews.query({
+				    review_object_id:curCommentId,
+				    page: 1,
+				    per_page: 20
+				}, function (e) {
+				    if (e.success) {
+				        Ti.API.info('commentID '+curCommentId+' has ' + e.reviews.length);
+				        for (var i = 0; i < e.reviews.length; i++) {
+	            			var review = e.reviews[i];
+					        var curComment = {
+				            	id: review.id,
+				            	topic_id: _topicId,
+				            	content: review.content,
+				            	rating: review.rating,
+				            	user:review.user,
+				            	response_to_object_id: curCommentId,
+				            	is_a_vate: review.custom_fields.is_a_vote,
+				            	updated_at: review.updated_at
+				           	};
+				        	commentsOfComments.push(curComment);   
+				        }
+				        
+				       	Ti.App.fireEvent("commentsLoadedComplete",{fetchedComments:commentsOfPost, fetchedCommentsOfComments:commentsOfComments});
+					} else {
+				        Ti.API.info('Getting CommentOfComment Error:\\n' +
+				            ((e.error && e.message) || JSON.stringify(e)));
+				    }
+				});
+			}
 	    } else {
 	        Ti.API.info('Getting Review Error:\\n' +
 	            ((e.error && e.message) || JSON.stringify(e)));
@@ -50,60 +90,9 @@ exports.commentACS_createCommentOfTopic = function(_comment,_topicId) {
 }
 
 exports.commentACS_createVoteOfComment = function(_voteScore,_commentId) {
-	//connecting with Cloud
-	//saving user action to CustomObjects, UserVotes class
-	Cloud.Objects.create({
-	    classname: 'UserVotes',
-	    fields: {
-	        targeted_comment_id: _commentId,
-	        vote: _voteScore
-	    }
-	}, function (e) {
-	    if (e.success) {
-	        alert('Success saving user vote: ');
-	    } else {
-	        alert('Error:\\n' +
-	            ((e.error && e.message) || JSON.stringify(e)));
-	    }
-	});
-	
-	//need to update that comment with a new rating +/- 1 -->using KeyValue Par, increment function
-	//can't use rating, since we need to make a call to get the currentvalue...too async
-	
-	//step 1. need to check if KeyValue pair for this commentId exists or not, 
-	//if already exsits, run increment function, if no--> create a KeyValue pair with key=commentId, value=1
-	Cloud.KeyValues.get({
-	    name: _commentId
-	}, function (e) {
-	    if (e.success) { //value pair exists, do the increment
-	       Cloud.KeyValues.increment({  //PROBLEM HERE..ONLY ALLOW INCR OF THE KEYVALUE OWNER T__T
-			    name: _commentId,
-			    value: _voteScore // +1 or -1..upvote or downvote
-			}, function (e) {
-			     if (e.success) {
-			         var keyvalue = e.keyvalues[0];
-			         Ti.API.info('Success incr: name: ' + keyvalue.name + ', value: ' + keyvalue.value);
-			     } else {
-			         Ti.API.info('Error: cannot incr rating of comment' +
-			             ((e.error && e.message) || JSON.stringify(e)));
-			     }
-			});
-	    } else { //value pair doesn't exists, create a new one
-	       Cloud.KeyValues.set({
-			    name: _commentId,
-			    value: 1
-			}, function (e) {
-			    if (e.success) {
-			        Ti.API.info('create a new keyvalue pair for commentId: '+_commentId);
-			    } else {
-			        Ti.API.info('Error: cannot create new keyvalue pair->' +
-			            ((e.error && e.message) || JSON.stringify(e)));
-			    }
-			});
-	    }
-	});
+	Ti.API.info("will create Review object to represent the voteup/down");
 }
 
 exports.commentACS_getAllVotesOfUser = function(_userId) {
-	Ti.API.info("be will implemented...");
+	Ti.API.info("may be no need to implement");
 }

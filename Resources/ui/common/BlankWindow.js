855915cc1d7f5cbd06571f0b4e4860d65218ed3c
Panisa <pokepokeru@hotmail.com>
2012-07-24 17:36:46 +0700

    blank window update

--- Resources/ui/common/BlankWindow.js
+++ Resources/ui/common/BlankWindow.js
@@ -3,12 +3,19 @@ var BlankWindow = function() {
 	//UI STUFF
 	var win = Ti.UI.createWindow({
 		backgroundColor:'transparent',
-		backgroundImage: '/images/admin/cb_back.png',
-		barColor: '#398bb0',
+		//backgroundImage: '/images/admin/cb_back.png',
+		//barColor: '#398bb0',
 		layout: 'vertical',
 		tabBarHidden: true,
 		navBarHidden: false
 	});
+	win.backgroundGradient = {
+		type: 'linear',
+		startPoint: { x: '0%', y: '0%' },
+		endPoint: { x: '0%', y: '100%' },
+		colors: [{ color: '#d2d1d0', offset: 0.0}, { color: '#fffefd', offset: 1.0 }]
+	};		
+		
 		
 	return win;
 };
